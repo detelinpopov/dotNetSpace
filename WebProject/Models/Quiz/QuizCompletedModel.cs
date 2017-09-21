601482fea5c1cd964452948ccf55242548d13819
detelinpopov <detelin.popov@pariplaydev.com>
2017-09-21 16:47:51 +0300

    Updates

--- WebProject/Models/Quiz/QuizCompletedModel.cs
+++ WebProject/Models/Quiz/QuizCompletedModel.cs
@@ -11,5 +11,26 @@ namespace WebProject.Models.Quiz
         public string TimeSpentText { get; set; }
 
         public int TotalQuestionsCount { get; set; }
+
+        public string UserLevel
+        {
+            get
+            {
+                var numberOfMistakes = TotalQuestionsCount - NumberOfCorrectAnswers;
+                if (numberOfMistakes <= 2)
+                {
+                    return Constantns.LevelSuperhero;
+                }
+                if (numberOfMistakes <= 5)
+                {
+                    return Constantns.LevelGood;
+                }
+                if (numberOfMistakes <= 10)
+                {
+                    return Constantns.LevelMediocre;
+                }
+                return Constantns.LevelPoor;
+            }
+        }
     }
 }
