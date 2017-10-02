300b81c9d9fc66e6ca715ee4510b86cd53a8e90f
detelinpopov <detelin.popov@pariplaydev.com>
2017-10-02 18:29:45 +0300

    Added RoboLogic

---
+++ WebProject/Scripts/robot.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $('#editor').hide();
+    setTimeout(function() {
+            toggleSpeechBubbleDisplay("But I want to learn how to code.");
+            setTimeout(function() {
+                    toggleSpeechBubbleDisplay("Actually I know some JavaScript and jQuery.");
+                    setTimeout(function() {
+                        toggleSpeechBubbleDisplay("Please help me to update my site!");
+                        setTimeout(function () {                              
+                            $('#editor').show();
+                                var editor = ace.edit("editor");
+                                editor.setTheme("ace/theme/monokai");
+                                editor.getSession().setMode("ace/mode/javascript");
+                            },
+                                5000);
+                        },
+                        5000);
+                },
+                5000);
+        },
+        5000);
+});
+
+function toggleSpeechBubbleDisplay(text) {
+    $(".speech-bubble").hide();
+    $(".speech-bubble").text(text);
+    $(".speech-bubble").fadeIn(2000);
+}
