200aa3f96129087af9ccb12d2b20a927845b4b39
detelinpopov <detelin.popov@pariplaydev.com>
2018-01-03 17:10:00 +0200

    Updates

--- WebProject/obj/Release/Package/PackageTmp/Scripts/index.js
+++ WebProject/obj/Release/Package/PackageTmp/Scripts/index.js
@@ -1,6 +1,17 @@
 ﻿$(document).ready(function() {
     var hdnUserMessageValue = $("[name=hdnUserMessage").val();
     if (hdnUserMessageValue.length > 0) {
-        $("#messageDialog").show(2000);       
+        $("#messageDialog").show(2000);
     }
+
+    $("#showQuizDescriptionSections").click(function() {
+        var hideSections = $(this);
+        if (hideSections.is(":checked")) {
+            $(".quiz-description").show("slow");
+            $(".quiz-description-category").show("slow");
+        } else {
+            $(".quiz-description").hide("slow");
+            $(".quiz-description-category").hide("slow");
+        }
+    });
 });
