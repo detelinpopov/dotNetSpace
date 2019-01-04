205e64b9c40f3632b195a33355a5ab04774f5c78
detelinpopov <detelin.popov@pariplaydev.com>
2019-01-04 14:03:08 +0200

    Updates

--- WebProject/Scripts/index.js
+++ WebProject/Scripts/index.js
@@ -71,4 +71,62 @@
     $(".start-quiz-button").click(function() {
         $(".load-indicator-container").fadeIn();
     });
+
+    var showcase = $("#showcase");
+    showcase.Cloud9Carousel({
+        yPos: 42,
+        yRadius: 48,
+        mirrorOptions: {
+            gap: 12,
+            height: 0.2
+        },
+        buttonLeft: $(".nav > .left"),
+        buttonRight: $(".nav > .right"),
+        autoPlay: true,
+        bringToFront: true,
+        onRendered: showcaseUpdated,
+        onLoaded: function () {
+            showcase.css("visibility", 'visible');
+            showcase.css("display", 'none');
+            showcase.fadeIn(1500);
+        }
+    });
+
+    function showcaseUpdated(showcase) {
+        var title = $("#item-title").html(
+            $(showcase.nearestItem()).attr("alt")
+        );
+
+        var c = Math.cos((showcase.floatIndex() % 1) * 2 * Math.PI);
+        title.css("opacity", 0.5 + (0.5 * c));
+    }
+
+    var showcaseJavaScript = $("#showcase-javascript");
+    showcaseJavaScript.Cloud9Carousel({
+        yPos: 42,
+        yRadius: 48,
+        mirrorOptions: {
+            gap: 12,
+            height: 0.2
+        },
+        buttonLeft: $(".nav > .left"),
+        buttonRight: $(".nav > .right"),
+        autoPlay: true,
+        bringToFront: true,
+        onRendered: showcaseUpdatedJs,
+        onLoaded: function () {
+            showcaseJavaScript.css("visibility", 'visible');
+            showcaseJavaScript.css("display", 'none');
+            showcaseJavaScript.fadeIn(1500);
+        }
+    });
+
+    function showcaseUpdatedJs(showcaseJavaScript) {
+        var title = $("#item-title").html(
+            $(showcaseJavaScript.nearestItem()).attr("alt")
+        );
+
+        var c = Math.cos((showcaseJavaScript.floatIndex() % 1) * 2 * Math.PI);
+        title.css("opacity", 0.5 + (0.5 * c));
+    }
 });
