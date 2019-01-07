1053a73f2986ba585e79f55572323ec69039c55d
detelinpopov <detelin.popov@pariplaydev.com>
2019-01-07 13:59:02 +0200

    u

--- WebProject/obj/Release/Package/PackageTmp/Scripts/index.js
+++ WebProject/obj/Release/Package/PackageTmp/Scripts/index.js
@@ -36,7 +36,7 @@
         }
     }
 
-    $(".footer").click(function() {
+    $("#showcase").click(function() {
         $("html, body").animate({ scrollTop: 0 }, 2000);
     });
 
@@ -71,4 +71,31 @@
     $(".start-quiz-button").click(function() {
         $(".load-indicator-container").fadeIn();
     });
+
+    var showcase = $(".showcase");
+    showcase.Cloud9Carousel({
+        yPos: 42,
+        yRadius: 48,
+        mirrorOptions: {
+            gap: 12,
+            height: 0.2
+        },
+        buttonLeft: $(".nav > .left"),
+        buttonRight: $(".nav > .right"),
+        autoPlay: true,
+        bringToFront: true,
+        onRendered: showcaseUpdated,
+        onLoaded: function () {
+         
+        }
+    });
+
+    function showcaseUpdated(showcase) {
+        var title = $("#item-title").html(
+            $(showcase.nearestItem()).attr("alt")
+        );
+
+        var c = Math.cos((showcase.floatIndex() % 1) * 2 * Math.PI);
+        title.css("opacity", 0.5 + (0.5 * c));
+    }   
 });
